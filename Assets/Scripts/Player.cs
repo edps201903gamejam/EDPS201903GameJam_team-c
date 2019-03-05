771239e50e0d352131443dacc3e231dec56b09ed
Gon <gon27.tk08@gmail.com>
2019-03-05 17:34:50 +0900

    テストコミット

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+
+	private Rigidbody rb;
+
+	void Start (){
+		rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		
+		
+	}
+
+	
+}
