7d5b9c406b7c9c7d2665c5eae4d197aa84fd96f0
Gon <gon27.tk08@gmail.com>
2019-03-06 10:35:06 +0900

    MainSceneを追加。 プレイヤーが動くようにした。

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,18 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour{
+public class Player : MonoBehaviour
+{
+	private float speed = 5.0f;
+	private float jumpSpeed = 8.0f;
 
-	private Rigidbody rb;
+	private Rigidbody2D rd;
 
+
+	private bool isGround = false;
+	
 	void Start (){
-		rb = gameObject.GetComponent<Rigidbody>();
+		rd = gameObject.GetComponent<Rigidbody2D>();
 	}
 	
 	void Update () {
-		
-		
+		Move();
+		Jump();
 	}
+
+	void Move()
+	{
+		float x = Input.GetAxisRaw("Horizontal");
+
+		rd.velocity = new Vector2(x * speed, rd.velocity.y);
+	}
+
+	void Jump()
+	{
+		if (isGround)
+		{
+//			Debug.Log();
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				rd.velocity = Vector2.up.normalized * jumpSpeed;
+				isGround = false;
+			}
+			
+		}
+	}
+	
+	void OnCollisionStay2D(Collision2D c)
+	{
+		if (c.gameObject.tag == "Block")
+		{
+			if (!isGround)
+			{
+				isGround = true;
+			}
+		}
+	}
+	
 
 	
 }
